1a303789e50c337252eebcbf706badad76a7923a
Kristian <kristiangrd131@gmail.com>
2024-11-23 11:26:20 -0500

    Update AreaPerBaseYAltura.py

--- GitHub/AreaPerBaseYAltura.py
+++ GitHub/AreaPerBaseYAltura.py
@@ -1,6 +1,7 @@
 def arepe(base, altura):
     peri=2*base + 2*altura
     area=base*altura
+    perimetro=peri
     return perimetro, area
 base=float(input('Digite el valor de la base del rectangulo'))
 altura=float(input('Digite la alutra del rectangulo'))
